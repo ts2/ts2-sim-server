7bb40fd026b2a3a2c0f5209cac255c31cda6c700
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2019-05-16 09:51:26 +0200

    Added simulation isStarted to UI templates

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cp ts2-sim-server docker/
 cp simulation/testdata/demo.json docker/
-docker build -t ts2simulator/sim-server docker
+docker build -t ts2simulator/sim-server --no-cache docker
